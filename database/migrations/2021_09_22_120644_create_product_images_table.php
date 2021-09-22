bcc7aea0049907484680317e2980a7cf6bbb8f82
gevorgchobanyan <gevorg.chobanyan22@gmail.com>
2021-09-22 08:56:53 -0400

    added migrations: options,options_value, products_option, images, product_images

---
+++ database/migrations/2021_09_22_120644_create_product_images_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('image_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('product_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product_images', function (Blueprint $table)
+        {
+            $table->dropForeign(['image_id']);
+            $table->dropForeign(['product_id']);
+
+        });
+        Schema::dropIfExists('product_images');
+    }
+}
