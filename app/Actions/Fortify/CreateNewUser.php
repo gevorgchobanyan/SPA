3034f393b2956b21783a85693a0304d79f4b005b
gevorgchobanyan <gevorg.chobanyan22@gmail.com>
2021-09-28 04:24:00 -0400

    added name field to register vue

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -35,6 +35,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'surname' => $input['surname'],
             'password' => Hash::make($input['password']),
         ]);
     }
